44f409dd1f32b5894c250e6022c3b5bccc3e7154
Patrick Spettel <patrick.spettel@aon.at>
2011-08-08 13:52:02 +0200

    finished command line view

--- src/connectfour/core.clj
+++ src/connectfour/core.clj
@@ -4,10 +4,15 @@
   (:gen-class))
 
 (defn -main [& args]
-  (let [model (make-model 6 7)
+  (let [model (make-model 4 4)
         view (make-view)]
     (set-initial-player model player1)
-    (render-model view model)
-    (throw-game-piece model 0)
-    (render-model view model)))
+    (loop [finished false]
+      (if finished
+        (render-model view model)
+        (do
+          (render-model view model)
+          (throw-game-piece model (next-turn view model))
+          (change-player model)
+          (recur (not (nil? (winner model)))))))))
 
--- src/connectfour/view/cliview.clj
+++ src/connectfour/view/cliview.clj
@@ -1,5 +1,12 @@
 (ns connectfour.view.cliview
-    (:use [connectfour.model.model]))
+  (:use [connectfour.model.model]))
+
+(def player-reprs {player1 \O player2 \X no-player \.})
+(def player-names {
+                   player1 "Player 1 (O)"
+                   player2 "Player 2 (X)"
+                   no-player "No Player (.)"
+                   })
 
 (defn make-view
   "Creates a new view. In the case of a CLI view there is
@@ -11,14 +18,42 @@
   "Displays the model."
   [view model]
   (let [n-rows (n-rows model)
-        n-cols (n-cols model)]
+        n-cols (n-cols model)
+        win (winner model)]
+    (if (nil? win)
+      (do
+        (print "It is ")
+        (print (player-names (curr-player model)))
+        (println "'s turn."))
+      (do
+        (print "Game finished. ")
+        (print (player-names win))
+        (println " is the winner.")))
     (doseq [r (range (dec n-rows) -1 -1)]
       (println
        (apply str
               (for [c (range n-cols)]
                 (let [player (player-at-row-col model r c)]
-                  (cond
-                   (= player player1) \O
-                   (= player player2) \X
-                   true \.))))))))
+                  (player-reprs player))))))))
+
+(defn next-turn
+  "Returns the column where the user wants to insert the new piece.
+  Asks the user again as long as a wrong number is entered."
+  [view model]
+  (loop [col nil]
+    (if (not (nil? col))
+      col
+      (do
+        (print "Please enter the column to insert the piece ")
+        (print "(0-")
+        (print (dec (n-cols model)))
+        (println "): ")
+        (let [c (try
+                  (Integer/parseInt (read-line))
+                  (catch NumberFormatException _ nil))]
+          (if (or (nil? c) (< c 0) (>= c (n-cols model)))
+            (do
+              (println "Invalid number entered. Try again!")
+              (recur nil))
+            (recur c)))))))
 
